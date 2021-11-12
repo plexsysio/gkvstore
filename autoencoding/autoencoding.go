d538a25671ae35ba6b6b4cfe591ca7dea5cdf0c3
alok <aloknerurkar@no-reply.com>
2021-11-12 19:02:59 +0530

    feat: use struct tags and fields instead of interface

--- autoencoding/autoencoding.go
+++ autoencoding/autoencoding.go
@@ -3,6 +3,9 @@ package autoencoding
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
+	"reflect"
+	"unicode"
 
 	"github.com/plexsysio/gkvstore"
 	"github.com/plexsysio/gkvstore/autoencoding/internal/gobpack"
@@ -13,50 +16,223 @@ import (
 type Encoding int
 
 const (
-	JSON Encoding = iota
+	Invalid Encoding = iota
+	JSON
 	Gob
 	MsgPack
 	Protobuf
 )
 
-type Item interface {
-	Encoding() Encoding
-	GetNamespace() string
-	GetID() string
-}
-
 type item struct {
-	Item
+	val       interface{}
+	id        string
+	namespace string
+	encoding  Encoding
 }
 
-func New(val Item) gkvstore.Item {
-	return &item{Item: val}
+type itemWithTimeTracker struct {
+	*item
+	created string
+	updated string
+}
+
+func getEncoding(f reflect.StructField) Encoding {
+	if _, ok := f.Tag.Lookup("protobuf"); ok {
+		fmt.Println("Using protobuf")
+		return Protobuf
+	}
+	if _, ok := f.Tag.Lookup("json"); ok {
+		fmt.Println("Using JSON")
+		return JSON
+	}
+	if _, ok := f.Tag.Lookup("msgpack"); ok {
+		fmt.Println("Using MsgPack")
+		return MsgPack
+	}
+	fmt.Println("Using Gob")
+	return Gob
+}
+
+func newItem(val interface{}) (gkvstore.Item, error) {
+	foundId, foundNs, foundCreated, foundUpdated := false, false, false, false
+	id, ns, created, updated := "", "", "", ""
+
+	rv := reflect.ValueOf(val)
+	if rv.Kind() != reflect.Ptr && rv.Kind() != reflect.Interface {
+		return nil, errors.New("incorrect value type: use pointer")
+	}
+	t := rv.Elem().Type()
+
+	var encoding Encoding
+	// Use first exported field for encoding
+	for i := 0; i < t.NumField(); i++ {
+		if unicode.IsUpper(rune(t.Field(i).Name[0])) {
+			encoding = getEncoding(t.Field(i))
+			break
+		}
+	}
+	if encoding == 0 {
+		return nil, errors.New("no exported field")
+	}
+
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		if field.Name == "ID" || field.Name == "Id" {
+			if foundId {
+				return nil, errors.New("duplicate ID field configured")
+			}
+			foundId = true
+			id = field.Name
+		}
+		if field.Name == "Namespace" {
+			if foundNs {
+				return nil, errors.New("duplicate Namespace field configured")
+			}
+			foundNs = true
+			ns = field.Name
+		}
+		if field.Name == "Created" || field.Name == "CreatedAt" {
+			if foundCreated {
+				return nil, errors.New("duplicate Created field configured")
+			}
+			foundCreated = true
+			created = field.Name
+		}
+		if field.Name == "Updated" || field.Name == "UpdatedAt" {
+			if foundUpdated {
+				return nil, errors.New("duplicate Updated field configured")
+			}
+			foundUpdated = true
+			updated = field.Name
+		}
+		if val, ok := field.Tag.Lookup("aenc"); ok {
+			if val == "id" {
+				if foundId && id != field.Name {
+					return nil, errors.New("duplicate ID field configured")
+				}
+				foundId = true
+				id = field.Name
+			}
+			if val == "namespace" && !foundNs {
+				if foundNs && ns != field.Name {
+					return nil, errors.New("duplicate Namespace field configured")
+				}
+				foundNs = true
+				ns = field.Name
+			}
+			if val == "created" {
+				if foundCreated && created != field.Name {
+					return nil, errors.New("duplicate Created field configured")
+				}
+				foundCreated = true
+				created = field.Name
+			}
+			if val == "updated" {
+				if foundUpdated && updated != field.Name {
+					return nil, errors.New("duplicate Updated field configured")
+				}
+				foundUpdated = true
+				updated = field.Name
+			}
+		}
+	}
+
+	if !foundId || !foundNs {
+		return nil, errors.New("ID and Namespace not configured")
+	}
+
+	if foundCreated && foundUpdated {
+		return &itemWithTimeTracker{
+			item: &item{
+				id:        id,
+				namespace: ns,
+				encoding:  encoding,
+				val:       val,
+			},
+			created: created,
+			updated: updated,
+		}, nil
+	}
+
+	return &item{
+		id:        id,
+		namespace: ns,
+		encoding:  encoding,
+		val:       val,
+	}, nil
+}
+
+func New(val interface{}) (gkvstore.Item, error) {
+	return newItem(val)
+}
+
+func MustNew(val interface{}) gkvstore.Item {
+	it, err := newItem(val)
+	if err != nil {
+		panic(err.Error())
+	}
+	return it
+}
+
+func (i *item) GetNamespace() string {
+	v := reflect.ValueOf(i.val).Elem()
+	return v.FieldByName(i.namespace).String()
+}
+
+func (i *item) GetID() string {
+	v := reflect.ValueOf(i.val).Elem()
+	return v.FieldByName(i.id).String()
+}
+
+func (i *item) SetID(id string) {
+	v := reflect.ValueOf(i.val).Elem()
+	v.FieldByName(i.id).SetString(id)
 }
 
 func (i *item) Marshal() ([]byte, error) {
-	switch i.Encoding() {
+	switch i.encoding {
 	case JSON:
-		return json.Marshal(i.Item)
+		return json.Marshal(i.val)
 	case Gob:
-		return gobpack.Marshal(i.Item)
+		return gobpack.Marshal(i.val)
 	case MsgPack:
-		return msgpack.Marshal(i.Item)
+		return msgpack.Marshal(i.val)
 	case Protobuf:
-		return proto.Marshal(i.Item.(proto.Message))
+		return proto.Marshal(i.val.(proto.Message))
 	}
 	return nil, errors.New("invalid encoding")
 }
 
 func (i *item) Unmarshal(buf []byte) error {
-	switch i.Encoding() {
+	switch i.encoding {
 	case JSON:
-		return json.Unmarshal(buf, i.Item)
+		return json.Unmarshal(buf, i.val)
 	case Gob:
-		return gobpack.Unmarshal(buf, i.Item)
+		return gobpack.Unmarshal(buf, i.val)
 	case MsgPack:
-		return msgpack.Unmarshal(buf, i.Item)
+		return msgpack.Unmarshal(buf, i.val)
 	case Protobuf:
-		return proto.Unmarshal(buf, i.Item.(proto.Message))
+		return proto.Unmarshal(buf, i.val.(proto.Message))
 	}
 	return errors.New("invalid encoding")
+}
+
+func (i *itemWithTimeTracker) GetCreated() int64 {
+	v := reflect.ValueOf(i.val).Elem()
+	return v.FieldByName(i.created).Int()
+}
+
+func (i *itemWithTimeTracker) GetUpdated() int64 {
+	v := reflect.ValueOf(i.val).Elem()
+	return v.FieldByName(i.updated).Int()
+}
+
+func (i *itemWithTimeTracker) SetCreated(val int64) {
+	v := reflect.ValueOf(i.val).Elem()
+	v.FieldByName(i.created).SetInt(val)
+}
+
+func (i *itemWithTimeTracker) SetUpdated(val int64) {
+	v := reflect.ValueOf(i.val).Elem()
+	v.FieldByName(i.updated).SetInt(val)
 }
--- autoencoding/autoencoding_test.go
+++ autoencoding/autoencoding_test.go
@@ -2,174 +2,263 @@ package autoencoding_test
 
 import (
 	"testing"
+	"time"
 
+	"github.com/plexsysio/gkvstore"
 	"github.com/plexsysio/gkvstore/autoencoding"
 	"github.com/plexsysio/gkvstore/autoencoding/internal/pbtest"
 )
 
-type jsonItem struct {
-	Name string
-	ID   string
-	Val1 int
-	Val2 string
+func TestNew(t *testing.T) {
+	t.Run("no id field", func(st *testing.T) {
+		type struct1 struct {
+			Namespace string
+			Val       string
+		}
+		_, err := autoencoding.New(&struct1{})
+		if err == nil {
+			st.Fatal("expected error for no id")
+		}
+	})
+	t.Run("no namespace field", func(st *testing.T) {
+		type struct1 struct {
+			Id  string
+			Val string
+		}
+		_, err := autoencoding.New(&struct1{})
+		if err == nil {
+			st.Fatal("expected error for no namespace")
+		}
+	})
+	t.Run("success without tags", func(st *testing.T) {
+		type struct1 struct {
+			Id        string
+			Namespace string
+			Val       string
+		}
+		it, err := autoencoding.New(&struct1{})
+		if err != nil {
+			st.Fatal("expected no error")
+		}
+		it.(gkvstore.IDSetter).SetID("testID")
+		if it.GetID() != "testID" {
+			st.Fatal("unable to set ID")
+		}
+	})
+	t.Run("success with tags", func(st *testing.T) {
+		type struct1 struct {
+			IdField        string `aenc:"id"`
+			NamespaceField string `aenc:"namespace"`
+			Val            string
+		}
+		it, err := autoencoding.New(&struct1{})
+		if err != nil {
+			st.Fatal("expected no error")
+		}
+		it.(gkvstore.IDSetter).SetID("testID")
+		if it.GetID() != "testID" {
+			st.Fatal("unable to set ID")
+		}
+	})
+	t.Run("success with tags and additional tags", func(st *testing.T) {
+		type struct1 struct {
+			IdField        string `json:"id" aenc:"id"`
+			NamespaceField string `json:"namespace,omitempty" aenc:"namespace"`
+			Val            string
+		}
+		_, err := autoencoding.New(&struct1{})
+		if err != nil {
+			st.Fatal("expected no error")
+		}
+	})
+	t.Run("duplicate field and tag", func(st *testing.T) {
+		type struct1 struct {
+			Id             string
+			IdField        string `aenc:"id"`
+			NamespaceField string `aenc:"namespace"`
+			Val            string
+		}
+		_, err := autoencoding.New(&struct1{})
+		if err == nil {
+			st.Fatal("expected error for duplicate id")
+		}
+	})
+	t.Run("MustNew panics on incorrect", func(st *testing.T) {
+		type struct1 struct {
+			IdField string
+			Ns      string
+			Val     string
+		}
+		panics := false
+		done := make(chan struct{})
+		go func() {
+			defer func() {
+				if r := recover(); r != nil {
+					panics = true
+				}
+				close(done)
+			}()
+			_ = autoencoding.MustNew(&struct1{})
+		}()
+		<-done
+		if !panics {
+			st.Fatal("expected MustNew to panic")
+		}
+	})
+	t.Run("timeTracker", func(st *testing.T) {
+		type struct1 struct {
+			ID        string
+			Namespace string
+			Created   int64
+			Updated   int64
+		}
+		it, err := autoencoding.New(&struct1{})
+		if err != nil {
+			st.Fatal("expected no error")
+		}
+		tt, ok := it.(gkvstore.TimeTracker)
+		if !ok {
+			st.Fatal("timetracker implementation expected")
+		}
+		ts := time.Now().UnixNano()
+		tt.SetCreated(ts)
+		tt.SetUpdated(ts)
+		if tt.GetCreated() != tt.GetUpdated() || tt.GetCreated() != ts {
+			st.Fatal("invalid timetracker implementation")
+		}
+	})
 }
 
-func (t *jsonItem) GetNamespace() string { return t.Name }
-
-func (t *jsonItem) GetID() string { return t.ID }
-
-func (t *jsonItem) Encoding() autoencoding.Encoding { return autoencoding.JSON }
-
-func TestJSONEncoding(t *testing.T) {
-	t1 := &jsonItem{
-		Name: "test1",
-		ID:   "1",
-		Val1: 100,
-		Val2: "test1 JSON",
-	}
-	ae1 := autoencoding.New(t1)
-
-	buf, err := ae1.Marshal()
-	if err != nil {
-		t.Fatalf("failed marshalling %v", err)
-	}
-
-	t.Log("Size of packing", len(buf))
-
-	t2 := &jsonItem{}
-	ae2 := autoencoding.New(t2)
-
-	err = ae2.Unmarshal(buf)
-	if err != nil {
-		t.Fatalf("failed unmarshaling %v", err)
-	}
-
-	if t1.Name != t2.Name || t1.ID != t2.ID || t1.Val1 != t2.Val1 || t1.Val2 != t2.Val2 {
-		t.Fatalf("read incorrect value expected %v found %v", t1, t2)
-	}
-}
-
-type msgpackItem struct {
-	Name string
-	ID   string
-	Val1 int
-	Val2 string
-}
-
-func (t *msgpackItem) GetNamespace() string { return t.Name }
-
-func (t *msgpackItem) GetID() string { return t.ID }
-
-func (t *msgpackItem) Encoding() autoencoding.Encoding { return autoencoding.MsgPack }
-
-func TestMsgpackEncoding(t *testing.T) {
-	t1 := &msgpackItem{
-		Name: "test1",
-		ID:   "1",
-		Val1: 100,
-		Val2: "test1 JSON",
-	}
-	ae1 := autoencoding.New(t1)
-
-	buf, err := ae1.Marshal()
-	if err != nil {
-		t.Fatalf("failed marshalling %v", err)
-	}
-
-	t.Log("Size of packing", len(buf))
-
-	t2 := &msgpackItem{}
-	ae2 := autoencoding.New(t2)
-
-	err = ae2.Unmarshal(buf)
-	if err != nil {
-		t.Fatalf("failed unmarshaling %v", err)
-	}
-
-	if t1.Name != t2.Name || t1.ID != t2.ID || t1.Val1 != t2.Val1 || t1.Val2 != t2.Val2 {
-		t.Fatalf("read incorrect value expected %v found %v", t1, t2)
-	}
-}
-
-type gobpackItem struct {
-	Name string
-	ID   string
-	Val1 int
-	Val2 string
-}
-
-func (t *gobpackItem) GetNamespace() string { return t.Name }
-
-func (t *gobpackItem) GetID() string { return t.ID }
-
-func (t *gobpackItem) Encoding() autoencoding.Encoding { return autoencoding.Gob }
-
-func TestGobpackEncoding(t *testing.T) {
-	t1 := &gobpackItem{
-		Name: "test1",
-		ID:   "1",
-		Val1: 100,
-		Val2: "test1 JSON",
-	}
-	ae1 := autoencoding.New(t1)
-
-	buf, err := ae1.Marshal()
-	if err != nil {
-		t.Fatalf("failed marshalling %v", err)
-	}
-
-	t.Log("Size of packing", len(buf))
-
-	t2 := &gobpackItem{}
-	ae2 := autoencoding.New(t2)
-
-	err = ae2.Unmarshal(buf)
-	if err != nil {
-		t.Fatalf("failed unmarshaling %v", err)
-	}
-
-	if t1.Name != t2.Name || t1.ID != t2.ID || t1.Val1 != t2.Val1 || t1.Val2 != t2.Val2 {
-		t.Fatalf("read incorrect value expected %v found %v", t1, t2)
-	}
-}
-
-type pbItem struct {
-	*pbtest.TestItem
-}
-
-func (t *pbItem) GetNamespace() string { return t.Name }
-
-func (t *pbItem) GetID() string { return t.Id }
-
-func (t *pbItem) Encoding() autoencoding.Encoding { return autoencoding.Protobuf }
-
-func TestProtobufEncoding(t *testing.T) {
-	t1 := &pbItem{
-		&pbtest.TestItem{
+func TestEncoding(t *testing.T) {
+	t.Run("JSON", func(st *testing.T) {
+		type jsonItem struct {
+			Name string `json:"name" aenc:"namespace"`
+			ID   string `json:"id"`
+			Val1 int    `json:"val1"`
+			Val2 string `json:"val2"`
+		}
+		t1 := &jsonItem{
 			Name: "test1",
-			Id:   "1",
+			ID:   "1",
 			Val1: 100,
 			Val2: "test1 JSON",
-		},
-	}
-	ae1 := autoencoding.New(t1)
+		}
+		ae1 := autoencoding.MustNew(t1)
 
-	buf, err := ae1.Marshal()
-	if err != nil {
-		t.Fatalf("failed marshalling %v", err)
-	}
+		buf, err := ae1.Marshal()
+		if err != nil {
+			st.Fatalf("failed marshalling %v", err)
+		}
 
-	t.Log("Size of packing", len(buf))
+		st.Log("Size of packing", len(buf))
 
-	t2 := &pbItem{&pbtest.TestItem{}}
-	ae2 := autoencoding.New(t2)
+		t2 := &jsonItem{}
+		ae2 := autoencoding.MustNew(t2)
 
-	err = ae2.Unmarshal(buf)
-	if err != nil {
-		t.Fatalf("failed unmarshaling %v", err)
-	}
+		err = ae2.Unmarshal(buf)
+		if err != nil {
+			st.Fatalf("failed unmarshaling %v", err)
+		}
 
-	if t1.Name != t2.Name || t1.Id != t2.Id || t1.Val1 != t2.Val1 || t1.Val2 != t2.Val2 {
-		t.Fatalf("read incorrect value expected %v found %v", t1, t2)
-	}
+		if t1.Name != t2.Name || t1.ID != t2.ID || t1.Val1 != t2.Val1 || t1.Val2 != t2.Val2 {
+			st.Fatalf("read incorrect value expected %v found %v", t1, t2)
+		}
+	})
+	t.Run("Gob", func(st *testing.T) {
+		type gobItem struct {
+			Name string `aenc:"namespace"`
+			ID   string
+			Val1 int
+			Val2 string
+		}
+		t1 := &gobItem{
+			Name: "test1",
+			ID:   "1",
+			Val1: 100,
+			Val2: "test1 JSON",
+		}
+		ae1 := autoencoding.MustNew(t1)
+
+		buf, err := ae1.Marshal()
+		if err != nil {
+			st.Fatalf("failed marshalling %v", err)
+		}
+
+		st.Log("Size of packing", len(buf))
+
+		t2 := &gobItem{}
+		ae2 := autoencoding.MustNew(t2)
+
+		err = ae2.Unmarshal(buf)
+		if err != nil {
+			st.Fatalf("failed unmarshaling %v", err)
+		}
+
+		if t1.Name != t2.Name || t1.ID != t2.ID || t1.Val1 != t2.Val1 || t1.Val2 != t2.Val2 {
+			st.Fatalf("read incorrect value expected %v found %v", t1, t2)
+		}
+	})
+	t.Run("MsgPack", func(st *testing.T) {
+		type msgpackItem struct {
+			Name string `msgpack:"name" aenc:"namespace"`
+			ID   string `msgpack:"id"`
+			Val1 int    `msgpack:"val1"`
+			Val2 string `msgpack:"val2"`
+		}
+		t1 := &msgpackItem{
+			Name: "test1",
+			ID:   "1",
+			Val1: 100,
+			Val2: "test1 JSON",
+		}
+		ae1 := autoencoding.MustNew(t1)
+
+		buf, err := ae1.Marshal()
+		if err != nil {
+			st.Fatalf("failed marshalling %v", err)
+		}
+
+		st.Log("Size of packing", len(buf))
+
+		t2 := &msgpackItem{}
+		ae2 := autoencoding.MustNew(t2)
+
+		err = ae2.Unmarshal(buf)
+		if err != nil {
+			st.Fatalf("failed unmarshaling %v", err)
+		}
+
+		if t1.Name != t2.Name || t1.ID != t2.ID || t1.Val1 != t2.Val1 || t1.Val2 != t2.Val2 {
+			st.Fatalf("read incorrect value expected %v found %v", t1, t2)
+		}
+	})
+	t.Run("Protobuf", func(st *testing.T) {
+		t1 := &pbtest.TestItem{
+			Namespace: "test1",
+			Id:        "1",
+			Val1:      100,
+			Val2:      "test1 JSON",
+		}
+		ae1 := autoencoding.MustNew(t1)
+
+		buf, err := ae1.Marshal()
+		if err != nil {
+			st.Fatalf("failed marshalling %v", err)
+		}
+
+		st.Log("Size of packing", len(buf))
+
+		t2 := &pbtest.TestItem{}
+		ae2 := autoencoding.MustNew(t2)
+
+		err = ae2.Unmarshal(buf)
+		if err != nil {
+			st.Fatalf("failed unmarshaling %v", err)
+		}
+
+		if t1.Namespace != t2.Namespace || t1.Id != t2.Id || t1.Val1 != t2.Val1 || t1.Val2 != t2.Val2 {
+			st.Fatalf("read incorrect value expected %v found %v", t1, t2)
+		}
+
+	})
 }
